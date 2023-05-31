e6932f5e21ff7e88aed9fba6c9a799181aa87393
Sergey Tszyu <Sergey.Tszyu@gmail.com>
2023-05-31 12:57:43 +0200

    Init

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Sergey Tszyu on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
